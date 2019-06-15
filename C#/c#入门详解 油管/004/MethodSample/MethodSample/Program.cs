47bbbb2ec4ba6f66c6d6abdf26118cce879fbe01
王玉涛 <qq5372@live.com>
2019-06-15 15:29:21 +0800

    油管 c# 刘 003 004

---
+++ C#/c#入门详解 油管/004/MethodSample/MethodSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MethodSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double x = Math.Sqrt(4);
+            double x = Math.Pow(2,3);
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ C#/c#入门详解 油管/004/propertySample/propertySample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventureWorksDW2012Entities proxy = new AdventureWorksDW2012Entities();
+            foreach (DimProduct p in proxy.DimProducts)
+            {
+                Console.WriteLine(p.EnglishProductName);
+            }
+            Console.WriteLine("=======================");
+            Console.WriteLine(proxy.DimProducts.Count());
+        }
+    }
+}
