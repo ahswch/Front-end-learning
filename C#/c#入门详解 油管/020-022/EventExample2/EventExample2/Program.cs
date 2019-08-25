b478e8ed2bb1a756ce676ab40a0d40c721bf2397
王玉涛 <qq5372@live.com>
2019-08-25 16:59:54 +0800

    add: 油管 c# 刘 020-022

---
+++ C#/c#入门详解 油管/020-022/EventExample2/EventExample2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EventExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            Controller controller = new Controller(form);
+            form.ShowDialog();
+        }
+    }
+    class Controller
+    {
+        private Form form;
+        public Controller(Form form)
+        {
+            if (form != null)
+            {
+                this.form = form;
+                this.form.Click += this.FormClicked;
+            }
+        }
+
+        private void FormClicked(object sender, EventArgs e)
+        {
+            this.form.Text = DateTime.Now.ToString();
+        }
+    }
+}
---
+++ C#/c#入门详解 油管/020-022/EventExample3/EventExample3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EventExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyForm form = new MyForm();
+            form.Click += form.FormClicked;
+            form.ShowDialog();
+        }
+    }
+    class MyForm : Form
+    {
+        internal void FormClicked(object sender, EventArgs e)
+        {
+            this.Text = DateTime.Now.ToString();
+        }
+    }
+}
