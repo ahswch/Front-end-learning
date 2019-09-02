cc7170da399e6c16361c7acb8146b95831ecc49f
王玉涛 <qq5372@live.com>
2019-09-02 23:32:42 +0800

    add: 油管 c# 刘 023-024

---
+++ C#/c#入门详解 油管/020-022/EventExample5/EventExample5/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventExample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Waiter waiter = new Waiter();
+            customer.Order += waiter.Action;
+            customer.Action();
+            customer.PayTheBill();
+        }
+    }
+
+    public class OrderEventArgs : EventArgs
+    {
+        public string DishName { get; set; }
+        public string Size { get; set; }
+    }
+
+    public delegate void OrderEventHandler(Customer customer, OrderEventArgs e);
+    public class Customer
+    {
+        // 简略声明
+        //public event OrderEventHandler Order;
+
+        //事件完整声明
+        private OrderEventHandler orderEventHandler;
+
+        public event OrderEventHandler Order
+        {
+            add
+            {
+                this.orderEventHandler += value;
+            }
+
+            remove
+            {
+                this.orderEventHandler -= value;
+            }
+        }
+
+        public double Bill { get; set; }
+        public void PayTheBill()
+        {
+            Console.WriteLine("I will pay ${0}.", this.Bill);
+        }
+
+        public void WalkIn()
+        {
+            Console.WriteLine("Walk into the restaurant.");
+        }
+
+        public void SitDown()
+        {
+            Console.WriteLine("Sit down.");
+        }
+
+        public void Think()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Let me think ...");
+                Thread.Sleep(1000);
+            }
+
+            // 完整
+            if (this.orderEventHandler != null)
+            {
+                OrderEventArgs e = new OrderEventArgs();
+                e.DishName = "Kongpao chicken";
+                e.Size = "large";
+                this.orderEventHandler.Invoke(this, e);
+            }
+
+            // 省略
+            //if (this.Order != null)
+            //{
+            //    OrderEventArgs e = new OrderEventArgs();
+            //    e.DishName = "Kongpao chicken";
+            //    e.Size = "large";
+            //    this.Order.Invoke(this, e);
+            //}
+        }
+
+        public void Action()
+        {
+            Console.ReadLine();
+            this.WalkIn();
+            this.SitDown();
+            this.Think();
+        }
+    }
+
+    public class Waiter
+    {
+        public void Action(Customer customer, OrderEventArgs e)
+        {
+            Console.WriteLine("I will serve you the dish - {0}", e.DishName);
+            double price = 10;
+            switch (e.Size)
+            {
+                case "small":
+                    price = price * 0.5;
+                    break;
+                case "large":
+                    price = price * 1.5;
+                    break;
+                default:
+                    break;
+            }
+
+            customer.Bill += price;
+        }
+    }
+}
---
+++ C#/c#入门详解 油管/020-022/EventExample6/EventExample6/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+
+namespace EventExample6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Waiter waiter = new Waiter();
+            customer.Order += waiter.Action;
+            customer.Action();
+            customer.PayTheBill();
+        }
+    }
+
+    public class OrderEventArgs : EventArgs
+    {
+        public string DishName { get; set; }
+        public string Size { get; set; }
+    }
+
+    public class Customer
+    {
+        // 使用原生EventHandler
+        public event EventHandler Order;
+
+        public double Bill { get; set; }
+        public void PayTheBill()
+        {
+            Console.WriteLine("I will pay ${0}.", this.Bill);
+        }
+
+        public void WalkIn()
+        {
+            Console.WriteLine("Walk into the restaurant.");
+        }
+
+        public void SitDown()
+        {
+            Console.WriteLine("Sit down.");
+        }
+
+        public void Think()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Let me think ...");
+                Thread.Sleep(1000);
+            }
+            this.OnOrder("Kongpao Chicken", "large");
+        }
+
+        protected void OnOrder(string dishName, string size)
+        {
+            if (this.Order != null)
+            {
+                OrderEventArgs e = new OrderEventArgs();
+                e.DishName = dishName;
+                e.Size = size;
+                this.Order.Invoke(this, e);
+            }
+        }
+
+        public void Action()
+        {
+            Console.ReadLine();
+            this.WalkIn();
+            this.SitDown();
+            this.Think();
+        }
+    }
+
+    public class Waiter
+    {
+        public void Action(object sender, EventArgs e)
+        {
+            Customer customer = sender as Customer;
+            OrderEventArgs orderInfo = e as OrderEventArgs;
+            Console.WriteLine("I will serve you the dish - {0}", orderInfo.DishName);
+            double price = 10;
+            switch (orderInfo.Size)
+            {
+                case "small":
+                    price = price * 0.5;
+                    break;
+                case "large":
+                    price = price * 1.5;
+                    break;
+                default:
+                    break;
+            }
+
+            customer.Bill += price;
+        }
+    }
+}
