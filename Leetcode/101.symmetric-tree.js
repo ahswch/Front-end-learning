976b79e5cd7c6d9a35965a9415095034ef19ef9d
ahswch <807320486@qq.com>
2019-10-31 15:15:23 +0800

    add: leetcode刷题更新

---
+++ Leetcode/101.symmetric-tree.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=101 lang=javascript
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (root === null) {
+      return true;
+    }
+
+    return isSymmetricHelper(root.left, root.right);
+};
+function isSymmetricHelper (left, right) {
+  if ((left === null && right !== null) || (left !== null && right === null)) {
+    return false;
+  }
+  if (left === null && right ===null) {
+    return true;
+  } else if (left.val !== right.val) {
+    return false;
+  }
+  return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+}
+// @lc code=end
+
